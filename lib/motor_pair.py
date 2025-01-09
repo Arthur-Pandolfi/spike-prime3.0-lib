b7cf67460a318e2a502ea729bc4a42db42b64e5e
Arthur Pandolfi <162211065+Arthur-Pandolfi@users.noreply.github.com>
2025-01-09 10:27:56 -0300

    Correção de Erros

--- lib/motor_pair.py
+++ lib/motor_pair.py
@@ -88,6 +88,73 @@ def move_for_degrees(pair: int, degrees: int, steering: int, *, velocity: int = 
     """
     pass
 
+def move_tank_for_degrees(pair: int, degrees: int, left_velocity: int, right_velocity: int, *, stop: int = motor.BRAKE, acceleration: int = 1000, deceleration: int = 1000) -> Awaitable:
+    """
+    move_tank_for_degrees(pair: int, degrees: int, left_velocity: int, right_velocity: int, *, stop: int = motor.BRAKE, acceleration: int = 1000, deceleration: int = 1000) -> Awaitable
+
+    Faça um Par de Motores realizar um movimento de tanque a uma velocidade constante até ser dado um novo comando.
+    Quando "aguardado" devolve um status do movimento que corresponde a uma das seguintes constantes do módulo do motor:
+
+    motor.READY
+    motor.RUNNING
+    motor.STALLED
+    motor.CANCELED
+    motor.ERROR
+    motor.DISCONNECTED
+
+    Parâmetros
+    pair: int
+    O slot do Par de Motores.
+    
+    degrees: int
+    O número de graus.
+    
+    left_velocity: int
+    A velocidade (grau/s) do motor esquerdo.
+    
+    right_velocity: int
+    A velocidade (grau/seg) do motor direito.
+    
+    Argumentos de palavra reservada opcionais:
+    stop: int
+    O comportamento do Motor depois de parar. Use as constantes no módulo motor.
+    
+    Os valores possíveis são:
+    motor.COAST para liberar o motor até uma parada.
+    motor.BRAKE para frear e continuar a frear após a parada.
+    motor.HOLD para orientar o motor a manter sua posição.
+    motor.CONTINUE para orientar o motor a continuar girando na velocidade atual até receber outro comando.
+    motor.SMART_COAST para frear o motor até parar e, depois, liberá-lo e compensar imprecisões no próximo comando.
+    motor.SMART_BRAKE para frear o motor, continuar freando até parar e então compensar imprecisões no próximo comando.
+    
+    acceleration: int
+    A aceleração (grau/s²) (1 - 10000).
+    
+    deceleration: int
+    A desaceleração (grau/s²), no intervalo (1 - 10000).
+    """
+
+def move_tank(pair: int, left_velocity: int, right_velocity: int, *, acceleration: int = 1000) -> None:
+    """
+    move_tank(pair: int, left_velocity: int, right_velocity: int, *, acceleration: int = 1000) -> None
+
+    Faça um Par de Motores realizar um movimento de tanque a uma velocidade constante até ser dado um novo comando.
+
+    Parâmetros
+    pair: int
+    O slot do Par de Motores.
+    
+    left_velocity: int
+    A velocidade (grau/s) do motor esquerdo.
+    
+    right_velocity: int
+    A velocidade (grau/seg) do motor direito.
+    
+    Argumentos de palavra reservada opcionais:
+    acceleration: int
+    A aceleração (grau/s²) (1 - 10000).
+    """
+
 def move_tank_for_time(pair: int, left_velocity: int, right_velocity: int, duration: int, *, stop: int = motor.BRAKE, acceleration: int = 1000, deceleration: int = 1000) -> Awaitable:
     """
     move_tank_for_time(pair: int, left_velocity: int, right_velocity: int, duration: int, *, stop: int = motor.BRAKE, acceleration: int = 1000, deceleration: int = 1000) -> Awaitable
